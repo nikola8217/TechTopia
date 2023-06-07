75d342e493edd8814438bc2e8f610f1f86042d17
nikola8217 <gileer404@gmail.com>
2023-06-07 11:19:09 +0200

    Products page

--- resources/js/pages/Products.vue
+++ resources/js/pages/Products.vue
@@ -1,11 +1,25 @@
 <template>
   <div>
-    <h1>Products page</h1>
+    <div class="collection mt-100">
+      <div class="container">
+        <div class="row">
+          <CategorySidebar />
+          <ProductList />
+          
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import ProductList from '../components/products/ProductList.vue';
+import CategorySidebar from '../components/products/CategorySidebar.vue';
+
 export default {
-  
+  components: {
+    ProductList,
+    CategorySidebar
+  }
 }
 </script>
